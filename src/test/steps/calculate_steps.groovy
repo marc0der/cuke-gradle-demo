c8aac2ef48d50694e693040eeecb2f3511f93957
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-27 18:27:03 +0000

    Adds imports to step defs.

--- src/test/steps/calculate_steps.groovy
+++ src/test/steps/calculate_steps.groovy
@@ -1,14 +1,3 @@
-import calc.Calculator
-
 import static cucumber.api.groovy.EN.*
 
-Given(~'^the input "([^"]*)"$') { String input ->
-    calculator = new Calculator(input:input)
-}
-
-When(~'^the calculator is run$') { ->
-    result = calculator.run()
-}
-Then(~'^the output should be "([^"]*)"$') { String output ->
-    assert result == output
-}
+import cucumber.api.PendingException
